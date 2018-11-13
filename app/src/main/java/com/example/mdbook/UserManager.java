68659791a9364f23d134b1ea93624fe8e3464bca
haondt <burghardtnoah@gmail.com>
2018-11-13 14:41:28 -0700

    set up new functions for creating users. implemented singleton functionality

--- app/src/main/java/com/example/mdbook/UserManager.java
+++ app/src/main/java/com/example/mdbook/UserManager.java
@@ -1,33 +1,41 @@
 package com.example.mdbook;
 
-import android.content.Context;
 
 public class UserManager {
 
+    static private UserManager userManager = null;
+    private ElasticsearchController esc;
+
+
     // create proper singleton of self
-    public static void initManager(Context context) {
-
-    }
-
-    // convert user to string through json
-    public static String userToString(User user) {
-        return("");
-    }
-
-    //convert string to user through json
-    public static User userFromString(String uString) {
-        return(new User("uid", "uphone", "uemail"));
-    }
-
-    // deconstruct user and update through elasticsearchcontroller
-    public void saveUser(User user) {
-
+    public static void initManager() {
+        if (userManager == null){
+            userManager = new UserManager();
+        }
     }
 
     // return singleton of self
     public static UserManager getManager() {
-        return(new UserManager());
+        if(userManager == null){
+            throw new IllegalStateException("UserManager has not been initialized!");
+        }
+        return userManager;
     }
+
+    // creates usermanager object
+    private UserManager(){
+        this.esc = ElasticsearchController.getController();
+    }
+
+    // create new patient
+    public void createPatient(String userID, String userPhone, String userEmail){
+
+    }
+    // create new Caregiver
+    public void createCaregiver(String userID, String userPhone, String userEmail){
+
+    }
+
 
     // deconstruct user and add through elasticsearchcontroller
     public void addUser(Patient u) {
@@ -47,8 +55,13 @@ public class UserManager {
     }
 
     // load data of user into new User object, return User object
-    public User loadUser(String Userid) {
-        return(new User("userID","userPhone", "userEmail"));
+    public User fetchUser (String Userid) {
+        return(new Patient("userID","userPhone", "userEmail"));
+    }
+
+    // deconstruct user and update through elasticsearchcontroller
+    public void saveUser(User user) {
+
     }
 
     // deconstruct user and remove all associated data through elasticsearchcontroller
