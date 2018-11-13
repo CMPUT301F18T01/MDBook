e34830e754ec8ff3da433cae3cbe7584d31cbbf3
haondt <burghardtnoah@gmail.com>
2018-11-13 14:40:49 -0700

    started filling in user class

--- app/src/main/java/com/example/mdbook/User.java
+++ app/src/main/java/com/example/mdbook/User.java
@@ -1,11 +1,45 @@
-
+/*
+ * User
+ *
+ * Version 0.0.1
+ *
+ * 2018-11-13
+ *
+ * Copyright (c) 2018. All rights reserved.
+ */
 package com.example.mdbook;
 
-public class User {
+/**
+ * Holds information relevant to a user, including a list of problems and contact information.
+ * Acts as superclass to Patient and Caregiver
+ *
+ * @author Noah Burghardt
+ * @see com.example.mdbook.Problem
+ * @see Patient
+ * @see Caregiver
+ * @see UserController
+ * @version 0.0.1
+ **/
+public abstract class User {
+
+    private String userID;
+    private String phone;
+    private String email;
+    private ProblemList problems;
 
     // generate a new user object
-    public User(String userID, String userPhone, String userEmail){
 
+    /**
+     * Generates a new user object, with a ProblemList of size 0.
+     * It is up to the UserManager to ensure that the user ID is unique
+     * @param userID
+     * @param userPhone
+     * @param userEmail
+     */
+    public User(String userID, String userPhone, String userEmail){
+        this.userID = userID;
+        this.phone = userPhone;
+        this.email = userEmail;
     }
 
     // return this users userID
