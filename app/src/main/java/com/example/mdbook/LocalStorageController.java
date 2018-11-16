42ae6218b7d2cdb0512eb223ee598289c8cb6a50
tcchan97 <tcchan@ualberta.ca>
2018-11-16 06:23:50 -0700

    added method to load back data

--- app/src/main/java/com/example/mdbook/LocalStorageController.java
+++ app/src/main/java/com/example/mdbook/LocalStorageController.java
@@ -5,6 +5,12 @@ import android.content.SharedPreferences;
 
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -40,14 +46,49 @@ class LocalStorageController {
         String stringProblems = problems.toJson(dataManager.getProblems());
         String stringRecords = records.toJson(dataManager.getRecords());
         String stringPhotos = photos.toJson(dataManager.getPhotos());
-        String stringAvailableIDS = availableIDs.toJson(dataManager.getAvailableID());
+        String stringAvailableIDs = availableIDs.toJson(dataManager.getAvailableID());
         editor.putString("patients", stringPatients);
         editor.putString("caregivers", stringCaregivers);
         editor.putString("problems", stringProblems);
         editor.putString("records", stringRecords);
         editor.putString("photos", stringPhotos);
-        editor.putString("availableIDS", stringAvailableIDS);
+        editor.putString("availableIDs", stringAvailableIDs);
         editor.apply();
+
+
+    }
+
+    public void loadData(){
+        SharedPreferences sharedPreferences = view.getSharedPreferences("shared preferences",MODE_PRIVATE);
+        Gson gson = new Gson();
+        String stringPatients = sharedPreferences.getString("patients", null);
+        String stringCaregivers = sharedPreferences.getString("caregivers", null);
+        String stringProblems = sharedPreferences.getString("problems", null);
+        String stringRecords = sharedPreferences.getString("records", null);
+        String stringPhotos = sharedPreferences.getString("photos", null);
+        String stringAvailableIDs = sharedPreferences.getString("availableIDs", null);
+
+
+        Type typeString = new TypeToken<HashMap<String, JSONObject>>(){}.getType();
+        Type typeInt = new TypeToken<HashMap<Integer, JSONObject>>(){}.getType();
+        Type typePhoto = new TypeToken<HashMap<Integer, Photo>>(){}.getType();
+
+
+
+        HashMap<String, JSONObject> Patients = gson.fromJson(stringPatients,typeString);
+        HashMap<String, JSONObject> Caregivers = gson.fromJson(stringCaregivers ,typeString);
+        HashMap<Integer, JSONObject> Problems = gson.fromJson(stringProblems,typeInt);
+        HashMap<Integer, JSONObject> Records = gson.fromJson(stringRecords,typeInt);
+        HashMap<Integer, Photo> Photos = gson.fromJson(stringPhotos,typePhoto);
+        //HashMap<String, JSONObject> AvailableIDs = gson.fromJson(stringAvailableIDs,type);
+
+
+        dataManager.setPatients(Patients);
+        dataManager.setCaregivers(Caregivers);
+        dataManager.setProblems(Problems);
+        dataManager.setRecords(Records);
+        dataManager.setPhotos(Photos);
+        //dataManager.setAvailableIDs(AvailableIDs);
 
 
     }
