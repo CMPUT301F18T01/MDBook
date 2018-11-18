3f518c0f687a99f8427b39a78372f3ea305649db
tcchan97 <tcchan@ualberta.ca>
2018-11-18 07:53:19 -0700

    added in ES components to data

--- app/src/main/java/com/example/mdbook/ElasticsearchController.java
+++ app/src/main/java/com/example/mdbook/ElasticsearchController.java
@@ -72,17 +72,81 @@ class ElasticsearchController {
     private void pushPatients(){
         HashMap<String, JSONObject> patients = dataManager.getPatients();
         for (String patiendID : patients.keySet()){
-            Index jestIndex = new Index.Builder(patients.get(patiendID)).index(index).type("patient").id(patiendID).build();
+            Index jestIndex = new Index.Builder(patients.get(patiendID)).index(index + "/patient").type("patient").id(patiendID).build();
+            Index saveID = new Index.Builder(patients.get(patiendID)).index(index + "/MetaDataPatient/list").type("MetadataPatient").id(patiendID).build();
             try {
                 client.execute(jestIndex);
+                client.execute(saveID);
             } catch (IOException e) {
                 throw new RuntimeException("Failed to upload patient " + patiendID, e);
             }
         }
     }
 
+    private void pushCaregivers(){
+        HashMap<String, JSONObject> caregivers = dataManager.getCaregivers();
+        for (String caregiversID : caregivers.keySet()){
+            Index jestIndex = new Index.Builder(caregivers.get(caregiversID)).index(index + "/caregiver").type("caregivers").id(caregiversID).build();
+            Index saveID = new Index.Builder(caregivers.get(caregiversID)).index(index + "/MetaDataCaregiver/list").type("MetadataCaregivers").id(caregiversID).build();
+            try{
+                client.execute(jestIndex);
+                client.execute(saveID);
+            } catch (IOException e){
+                throw  new RuntimeException("Failed to upload caregives " + caregiversID, e);
+            }
+        }
+    }
+
+    private void pushProblems(){
+        HashMap<Integer, JSONObject> problems = dataManager.getProblems();
+        for (Integer problemsID : problems.keySet()){
+            Index jestIndex = new Index.Builder(problems.get(problemsID)).index(index + "/problems").type("problems").id(problemsID.toString()).build();
+            Index saveID = new Index.Builder(problems.get(problemsID)).index(index + "/MetaDataProblems/list").type("MetadataProblems").id(problemsID.toString()).build();
+            try{
+                client.execute(jestIndex);
+                client.execute(saveID);
+            } catch (IOException e){
+                throw  new RuntimeException("Failed to upload problem " + problemsID.toString(), e);
+            }
+        }
+    }
+
+    private void pushRecords(){
+        HashMap<Integer, JSONObject> records = dataManager.getRecords();
+        for (Integer recordsID : records.keySet()){
+            Index jestIndex = new Index.Builder(records.get(recordsID)).index(index+"/records").type("records").id(recordsID.toString()).build();
+            Index saveID = new Index.Builder(records.get(recordsID)).index(index+"/MetaDataRecords/list").type("MetadataRecords").id(recordsID.toString()).build();
+            try{
+                client.execute(jestIndex);
+                client.execute(saveID);
+            } catch (IOException e){
+                throw  new RuntimeException("Failed to upload records" + recordsID.toString(), e);
+            }
+        }
+    }
+
+    private void pushPhotos(){
+        HashMap<Integer, Photo> photos = dataManager.getPhotos();
+        for (Integer photosID : photos.keySet()){
+            Index jestIndex = new Index.Builder(photos.get(photosID)).index(index +"/photos").type("photos").id(photosID.toString()).build();
+            Index saveID = new Index.Builder(photos.get(photosID)).index(index + "MetaDataPhotos/list").type("MetadataPhotos").id(photosID.toString()).build();
+            try{
+                client.execute(jestIndex);
+                client.execute(saveID);
+            } catch (IOException e){
+                throw  new RuntimeException("Failed to upload photo " + photosID.toString(), e);
+            }
+        }
+    }
+
+
     public void push() {
         this.pushPatients();
+        this.pushCaregivers();
+        this.pushProblems();
+        this.pushRecords();
+        this.pushPhotos();
+
     }
 
 
