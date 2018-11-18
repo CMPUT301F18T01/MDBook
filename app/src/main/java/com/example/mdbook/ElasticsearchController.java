6f93619936989fe726026202ca5361f4fea17b9c
tcchan97 <tcchan@ualberta.ca>
2018-11-17 17:52:22 -0700

    added in jest client:

--- app/src/main/java/com/example/mdbook/ElasticsearchController.java
+++ app/src/main/java/com/example/mdbook/ElasticsearchController.java
@@ -9,16 +9,25 @@ package com.example.mdbook;
  * Copyright (c) 2018. All rights reserved.
  */
 
+import com.searchly.jestdroid.DroidClientConfig;
+import com.searchly.jestdroid.JestClientFactory;
+import com.searchly.jestdroid.JestDroidClient;
+
 /**
  * Keeps DataManager in sync with cloud storage.
  *
  * @author Noah Burghardt
  * @version 0.0.1
  **/
+
+//http://cmput301.softwareprocess.es:8080/cmput301f18t01test
+//http://cmput301.softwareprocess.es:8080/cmput301f18t01
+
 class ElasticsearchController {
 
     private static ElasticsearchController elasticsearchController = null;
     private DataManager dataManager = DataManager.getDataManager();
+    private static JestDroidClient client;
 
     /**
      * @return Singleton instance of ElasticSearchController
@@ -30,7 +39,30 @@ class ElasticsearchController {
         return elasticsearchController;
     }
 
-    public void push() {
+
+
+    public static void verfiySettings(){
+        if (client == null){
+            DroidClientConfig.Builder builder = new DroidClientConfig.Builder("http://cmput301.softwareprocess.es:8080/cmput301f18t01");
+            DroidClientConfig config = builder.build();
+
+            JestClientFactory factory = new JestClientFactory();
+            factory.setDroidClientConfig(config);
+            client  = (JestDroidClient) factory.getObject();
+        }
     }
+
+
+
+
+
+
+
+    public void push() {
+
+
+    }
+
+
 }
 
