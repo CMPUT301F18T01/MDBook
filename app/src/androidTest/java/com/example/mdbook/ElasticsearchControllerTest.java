7745213c54f3c9282f4d78eb3971ce69c818acbe
Noah Burghardt <burghardtnoah@gmail.com>
2018-11-19 15:43:22 -0700

    finished es test - has bugs though

--- app/src/androidTest/java/com/example/mdbook/ElasticsearchControllerTest.java
+++ app/src/androidTest/java/com/example/mdbook/ElasticsearchControllerTest.java
@@ -10,12 +10,25 @@
 
 package com.example.mdbook;
 
+import com.searchly.jestdroid.DroidClientConfig;
+import com.searchly.jestdroid.JestClientFactory;
+
 import junit.framework.TestCase;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestResult;
+import io.searchbox.core.DocumentResult;
+import io.searchbox.core.Get;
 
 /**
  * test for elasticsearchcontroller
  * tests are a bit wordy, most of this stuff will be wrapped up cleanly in usermanager
- *
+ * In order for tests to run, Elasticsearch index must be at cmput301f18t01test
  * @author Noah Burghardt
  * @author James Aina
  *
@@ -23,15 +36,31 @@ import junit.framework.TestCase;
  **/
 public class ElasticsearchControllerTest extends  TestCase{
 
+    private String index = "cmput301f18t01test";
+
+    private JestClient getJestClient(){
+        JestClientFactory factory = new JestClientFactory();
+        factory.setDroidClientConfig(new DroidClientConfig
+                .Builder("http://cmput301.softwareprocess.es:8080")
+                .multiThreaded(true)
+                .defaultMaxTotalConnectionPerRoute(2)
+                .maxTotalConnection(20)
+                .build());
+        return factory.getObject();
+    }
+
     public void testAddPatientTask(){
         /* Set up testing environment */
         UserManager.initManager();
         UserManager userManager = UserManager.getManager();
         ElasticsearchController elasticsearchController = ElasticsearchController.getController();
-
+        JestClient client = this.getJestClient();
         /* Create and push new patient */
+        String patientID = "testPatientID";
+        String testPhone = "testPhone";
+        String testEmail = "testEmail";
         try {
-            Patient patient = userManager.createPatient("patientID", "test-phone", "testemail");
+            Patient patient = userManager.createPatient(patientID, "test-phone", "testemail");
         } catch (UserIDNotAvailableException e) {
             fail();
         }
@@ -39,6 +68,23 @@ public class ElasticsearchControllerTest extends  TestCase{
 
         // TODO: set up jest controller in test
         // verify the above patient is created at http://cmput301.softwareprocess.es:8080/cmput301f18t01test/patient/patientID
+        // fetch object at given url
+        DocumentResult result = null;
+        try {
+            result = client.execute(new Get.Builder(patientID,index)
+                    .type("patient")
+                    .build());
+            JSONObject resultJSON = result.getSourceAsObject(JSONObject.class);
+
+            assertEquals(testEmail, resultJSON.getString("email"));
+            assertEquals(testPhone, resultJSON.getString("phone"));
+        } catch (IOException e) {
+            fail();
+        } catch (JSONException e) {
+            fail();
+        }
+
 
     }
+
 }
