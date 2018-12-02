3151a95d456eadd1d53d327efaad0cd6f6097718
haondt <burghardtnoah@gmail.com>
2018-12-02 14:53:23 -0700

    added userdecomposer comments

--- app/src/main/java/com/example/mdbook/UserDecomposer.java
+++ app/src/main/java/com/example/mdbook/UserDecomposer.java
@@ -1,3 +1,12 @@
+/*
+ * UserDecomposer
+ *
+ * Version 1.0.0
+ *
+ * 2018-12-02
+ *
+ * Copyright (c) 2018. All rights reserved.
+ */
 package com.example.mdbook;
 
 import android.accounts.NetworkErrorException;
@@ -12,38 +21,48 @@ import java.util.Date;
 import java.util.HashMap;
 
 /**
- * Deconstructs user for saving to elasticsearch.
+ * Tools for converting user objects to and from JSONObjects.
+ * Data Structure:
+ * PatientID:
+ *      "phone": String
+ *      "email": String
+ *      "problems": ArrayList of problemIDs (ints)
+ * CaregiverID:
+ *      "phone": String
+ *      "email": String
+ *      "patients": ArrayList of patient userIDs (strings)
+ * ProblemID:
+ *      "title": String
+ *      "description": String
+ *      "comments": ArrayList of comments (strings)
+ *      "records": ArrayList of recordIDs (ints)
+ * RecordID:
+ *      "title": String
+ *      "date": Date
+ *      "description": String
+ *      "geoLocation": GeoLocation
+ *      "bodyLocation": BodyLocation
+ *      "photos": ArrayList of photoIDs (ints)
+ *      "comment": String
+ * PhotoID: Photo
  *
+ *
+ *
+ * @author Noah Burghardt
+ * @see User
+ * @version 1.0.0
  */
 public class UserDecomposer {
+
     /**
-     * PatientID:
-     *      "phone": String
-     *      "email": String
-     *      "problems": ArrayList of problemIDs (ints)
-     * CaregiverID:
-     *      "phone": String
-     *      "email": String
-     *      "patients": ArrayList of patient userIDs (strings)
-     * ProblemID:
-     *      "title": String
-     *      "description": String
-     *      "comments": ArrayList of comments (strings)
-     *      "records": ArrayList of recordIDs (ints)
-     * RecordID:
-     *      "title": String
-     *      "date": Date
-     *      "description": String
-     *      "geoLocation": GeoLocation
-     *      "bodyLocation": BodyLocation
-     *      "photos": ArrayList of photoIDs (ints)
-     *      "comment": String
-     * PhotoID: Photo
-     *
-     *
+     * Converts user object to decomposition. Relies on ElasticsearchController to provide IDs for
+     * objects before converting them to JSON.
+     * @see UserDecomposer.Decomposition
+     * @see User
+     * @param user The user (Patient or Caregiver) to be deconstructed.
+     * @return The decomposition of the user.
+     * @throws NetworkErrorException Thrown if no internet access is available.
      */
-
-
     public Decomposition decompose(User user) throws NetworkErrorException {
         Decomposition decomposition = new Decomposition(user.getUserID());
         ElasticsearchController elasticsearchController = ElasticsearchController.getController();
@@ -164,6 +183,13 @@ public class UserDecomposer {
         }
     }
 
+    /**
+     * Builds user object from decomposition.
+     * @see UserDecomposer.Decomposition
+     * @see User
+     * @param decomposition The decomposition to be turned into a user.
+     * @return The user built from the decomposition.
+     */
     public User compose(Decomposition decomposition){
         try {
             if (decomposition.getProblems() != null) {
@@ -253,6 +279,11 @@ public class UserDecomposer {
     }
 
 
+    /**
+     * Equivalent of a User object, but with its components converted to JSONObjects and grouped
+     * into HashMaps based on their types. Makes it easier to store in Elasticsearch.
+     * @see User
+     */
     public static class Decomposition {
 
         private String userid;
