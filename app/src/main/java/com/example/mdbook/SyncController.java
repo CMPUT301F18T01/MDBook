72f58b628028d589c6000f630cf4bf5c0e45528a
tcchan97 <tcchan@ualberta.ca>
2018-12-03 14:55:44 -0700

    easier implmentation

--- app/src/main/java/com/example/mdbook/SyncController.java
+++ app/src/main/java/com/example/mdbook/SyncController.java
@@ -36,56 +36,36 @@ public class SyncController extends BroadcastReceiver {
         dataManager = DataManager.getDataManager();
         decomposer = new UserDecomposer();
 
-
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
-        if(connectivityManager != null){
-            NetworkInfo[] networkInfo = connectivityManager.getAllNetworkInfo();
-            if (networkInfo !=null){
-                for (int i = 0; i< networkInfo.length;i++){
-                    if (networkInfo[i].getState() == NetworkInfo.State.CONNECTED){
-                        if (!isConnected){
-                            Toast.makeText(context,"Internet is available",Toast.LENGTH_SHORT).show();
-                            if (elasticsearchController.isConnected()){
-                                ArrayList<User> toupload = (ArrayList<User>) dataManager.getPushQueue().clone();
-                                for (User user1 : toupload){
-
-                                    UserDecomposer.Decomposition userDecomp;
-                                    try {
-                                        userDecomp = decomposer.decompose(user1);
-                                        if (user1.getClass() == Patient.class) {
-                                            if (elasticsearchController.pushPatient(userDecomp)) {
-                                                dataManager.removeFromQueue(user1);
-                                            }
-                                        }
-                                        else {
-                                            if (elasticsearchController.pushCaregiver(userDecomp)) {
-                                                dataManager.removeFromQueue(user1);
-                                            }
-                                        }
-
-                                    } catch (NetworkErrorException e) {
-                                        break;
-                                    }
-                                }
-                            }
-
-                            isConnected = true;
-                            break;
-
-
-                        }
-                    }
-                }
-            }
-
-
-
-
+        boolean isConnected = intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+        if(isConnected){
+            Toast.makeText(context,"Internet connection lost",Toast.LENGTH_SHORT).show();
 
         }
         else{
-            Toast.makeText(context,"No Internet",Toast.LENGTH_SHORT).show();
-            isConnected = false;
+            Toast.makeText(context,"Internet Available",Toast.LENGTH_SHORT).show();
+            if (elasticsearchController.isConnected()){
+                ArrayList<User> toupload = (ArrayList<User>) dataManager.getPushQueue().clone();
+                for (User user1 : toupload){
+
+                    UserDecomposer.Decomposition userDecomp;
+                    try {
+                        userDecomp = decomposer.decompose(user1);
+                        if (user1.getClass() == Patient.class) {
+                            if (elasticsearchController.pushPatient(userDecomp)) {
+                                dataManager.removeFromQueue(user1);
+                            }
+                        }
+                        else {
+                            if (elasticsearchController.pushCaregiver(userDecomp)) {
+                                dataManager.removeFromQueue(user1);
+                            }
+                        }
+
+                    } catch (NetworkErrorException e) {
+                        break;
+                    }
+                }
+            }
         }
     }
 }
