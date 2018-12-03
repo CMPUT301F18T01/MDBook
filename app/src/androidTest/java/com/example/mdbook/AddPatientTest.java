aa9ab4441d14c3e0e63f31ba60443a8806ef100b
rajkapadia <rkkapadi@ualberta.ca>
2018-12-03 14:20:10 -0700

    ..

---
+++ app/src/androidTest/java/com/example/mdbook/AddPatientTest.java
@@ -0,0 +1,75 @@
+package com.example.mdbook;
+
+import android.app.Activity;
+import android.support.test.rule.ActivityTestRule;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.robotium.solo.Solo;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+
+public class AddPatientTest {
+
+    private Solo solo;
+    private EditText editText;
+    private Button loginButton;
+    private TextView registerText;
+
+    @Rule
+    public ActivityTestRule<LoginActivity> activityTestRule= new ActivityTestRule(LoginActivity.class);
+
+
+    public void setUp() throws Exception{
+        solo = new Solo(getInstrumentation(), activityTestRule.getActivity());
+    }
+
+
+    @Test
+    public void testStart() throws Exception {
+        Activity activity = activityTestRule.getActivity();
+    }
+
+    @Test
+    public void testAddPatient()
+    {
+        solo = new Solo(getInstrumentation(), activityTestRule.getActivity());
+        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
+        solo.clickOnView(solo.getView(R.id.registerBtn));
+
+        solo.assertCurrentActivity("Wrong Activity", RegisterActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.etUserIDR), "rajkapadia");
+        solo.enterText((EditText) solo.getView(R.id.etEmail), "rajkapadia@test.com");
+        solo.enterText((EditText) solo.getView(R.id.etPhoneNumber), "0000000000");
+        solo.clickOnView(solo.getView(R.id.registerButton));
+
+        solo.goBackToActivity("LoginActivity");
+        solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
+        solo.clickOnView(solo.getView(R.id.registerBtn));
+
+        solo.assertCurrentActivity("Wrong Activity", RegisterActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.etUserIDR), "rajkapadiaCG");
+        solo.enterText((EditText) solo.getView(R.id.etEmail), "rajkapadia@test.com");
+        solo.enterText((EditText) solo.getView(R.id.etPhoneNumber), "0000000000");
+        CheckBox checkBox = (CheckBox) solo.getView(R.id.cgCheckBox);
+        solo.clickOnView(checkBox);
+        solo.clickOnView(solo.getView(R.id.registerButton));
+
+        solo.goBackToActivity("LoginActivity");
+        solo.enterText((EditText) solo.getView(R.id.etUserID), "rajkapadiaCG");
+        solo.clickOnView(solo.getView(R.id.loginButton));
+//
+//        solo.assertCurrentActivity("Wrong Activity", ListPatientActivity.class);
+//        FloatingActionButton  fab = (FloatingActionButton) solo.getView(R.id.gotoAddPatient);
+//        solo.clickOnView(fab);
+//
+//        solo.assertCurrentActivity("Wrong activity", AddRecordActivity.class);
+//
+
+    }
+}
