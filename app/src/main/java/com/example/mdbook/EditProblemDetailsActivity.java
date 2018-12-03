0f2755016a4616dc86b7ad5b658e611880cb8028
rajkapadia <rkkapadi@ualberta.ca>
2018-12-02 18:41:35 -0700

    minor UI changes for problems to view date

--- app/src/main/java/com/example/mdbook/EditProblemDetailsActivity.java
+++ app/src/main/java/com/example/mdbook/EditProblemDetailsActivity.java
@@ -10,16 +10,24 @@
 
 package com.example.mdbook;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Creates a view for editing the problems details
@@ -33,24 +41,27 @@ import org.w3c.dom.Text;
  *
  * @version 0.0.1
  **/
-public class EditProblemDetailsActivity extends AppCompatActivity {
+public class EditProblemDetailsActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
-    EditText editTitle;
-    TextView showDate;
-    EditText editDescription;
-    Button save;
-    Button cancel;
-    Problem problem;
-    int problemPos;
-    Patient patient;
+    private EditText editTitle;
+    private Button editDate;
+    private EditText editDescription;
+    private Button save;
+    private Button cancel;
+    private Problem problem;
+    private int problemPos;
+    private Patient patient;
+    private Date date;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_problem_details);
+
         // set the views and buttons appropriately by id's
         editTitle = findViewById(R.id.showTitle);
-        showDate = findViewById(R.id.showDate);
+        editDate = findViewById(R.id.datePickerEdit);
         editDescription = findViewById(R.id.editDescription);
         save = findViewById(R.id.save);
         cancel = findViewById(R.id.cancel);
@@ -61,7 +72,7 @@ public class EditProblemDetailsActivity extends AppCompatActivity {
         problem = patient.getProblems().get(problemPos);
         editTitle.setText(problem.getTitle());
         editDescription.setText(problem.getDescription());
-        showDate.setText(problem.getDate().toString());
+        editDate.setText(problem.getDate().toString());
 
         // Switches to the main activity upon the click of the save button
         save.setOnClickListener(new View.OnClickListener() {
@@ -80,6 +91,25 @@ public class EditProblemDetailsActivity extends AppCompatActivity {
                 backToMainPage();
             }
         });
+
+        editDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker(v);
+
+
+            }
+        });
+    }
+
+    /**
+     * Creates a DatePicker instance and lets the user select the date from
+     * calender that pops. This happens when
+     * @param v
+     */
+    public void showDatePicker(View v) {
+        DialogFragment newFragment = new AddProblemActivity.DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "date picker");
     }
 
 
@@ -90,6 +120,41 @@ public class EditProblemDetailsActivity extends AppCompatActivity {
         Intent intent = new Intent(EditProblemDetailsActivity.this, ListProblemActivity.class);
         startActivity(intent);
         this.finish();
+    }
+
+    private String setDate(Date date) {
+        String strDate;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yy");
+        strDate = dateFormat.format(date);
+        return strDate;
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+
+        view.setMaxDate(Calendar.getInstance().getTimeInMillis());
+        date = new Date(view.getYear(), view.getMonth(), view.getDayOfMonth());
+        String strDate = setDate(date);
+        editDate.setText(strDate);
+        problem.setDate(date);
+        Toast.makeText(EditProblemDetailsActivity.this, strDate, Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * A static fragment class required for proper DatePicker implementation
+     */
+    public static class DatePickerFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            return new DatePickerDialog(getActivity(),
+                    (DatePickerDialog.OnDateSetListener)
+                            getActivity(), year, month, day);
+        }
     }
 
 }
