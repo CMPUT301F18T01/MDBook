fc92e73d99028b6b173f7cf1c14f3bf7a57f81f1
rajkapadia <rkkapadi@ualberta.ca>
2018-12-02 22:31:20 -0700

    intent test

--- app/src/androidTest/java/com/example/mdbook/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/mdbook/LoginActivityTest.java
@@ -1,6 +1,8 @@
 package com.example.mdbook;
 
 import android.accounts.NetworkErrorException;
+import android.support.test.espresso.intent.matcher.IntentMatchers;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.rule.ActivityTestRule;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
@@ -11,37 +13,39 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 public class LoginActivityTest{
 
     @Rule
-    public ActivityTestRule<LoginActivity> loginActivityActivityTestRule = new ActivityTestRule<>(LoginActivity.class);
-    private LoginActivity LoginActivity = null;
+    public IntentsTestRule<LoginActivity> loginActivityActivityTestRule = new IntentsTestRule<>(LoginActivity.class);
 
     @Before
     public void setUp() throws Exception {
-        LoginActivity = loginActivityActivityTestRule.getActivity();
+        loginActivityActivityTestRule.getActivity();
+        Patient p = new Patient("rajkapadia", "1234567890", "test@test.com");
+        UserController.getController().loadUser(p);
+        Caregiver c = new Caregiver("rajkapadiaCG", "1234567890", "test@test.com");
+        UserController.getController().loadUser(c);
+
     }
 
     @Test
     public void login(){
-        String etUserID;
-        etUserID = "Shitake";
-        UserManager userManager = UserManager.getManager();
-        try {
-            if (userManager.login(etUserID)){
-                assertNotNull(etUserID);
-            }
-        } catch (NetworkErrorException e) {
-            e.printStackTrace();
-        }
+        loginActivityActivityTestRule.getActivity();
+        onView(withId(R.id.etUserID))
+                .perform(typeText("rajkapadia"), closeSoftKeyboard());
+        onView(withId(R.id.loginButton))
+                .perform(click());
+        intended(IntentMatchers.hasComponent(ListProblemActivity.class.getName()));
 
     }
 
-    @After
-    public void tearDown() throws Exception {
-        LoginActivity = null;
-    }
 }
